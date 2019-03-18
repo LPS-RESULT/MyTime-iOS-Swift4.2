41f55b408b23036fb9eae8749ce06a7b2b2ddfb3
Timothy Santiago <santiagotimothy@gmail.com>
2019-03-18 13:29:41 +1300

    Finished login ui

--- MyTimeSW/Containers/Sign In/SignInViewController.swift
+++ MyTimeSW/Containers/Sign In/SignInViewController.swift
@@ -20,9 +20,18 @@ class SignInViewController: UIViewController {
         setupViews()
     }
     
+    @objc func signInButtonClicked(sender: UIButton) {
+        
+    }
+    
     func setupViews() {
-//        self.view.backgroundColor = LPSColors.primary
-        self.view.backgroundColor = UIColor.black
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [LPSColors.loginBackgroundGradientStart.cgColor, LPSColors.loginBackgroundGradientEnd.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
         
         // Unwrap self.view
         let view = self.view!
@@ -55,7 +64,123 @@ class SignInViewController: UIViewController {
         topText.topAnchor.constraint(equalTo: topSubtext.bottomAnchor, constant: -2).isActive = true
         topText.leadingAnchor.constraint(equalTo: topSubtext.leadingAnchor).isActive = true
         
+        let usernameIcon = UIImageView()
+        let personIcon = MaterialDesignSymbol(text:MaterialDesignIcon.person24px, size:20)
+        personIcon.addAttribute(attributeName: NSAttributedString.Key.foregroundColor, value: UIColor.white)
+        usernameIcon.image = personIcon.image(size: CGSize(width: 20, height: 20))
+        usernameIcon.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(usernameIcon)
+        usernameIcon.topAnchor.constraint(equalTo: topText.bottomAnchor, constant: 30).isActive = true
+        usernameIcon.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        usernameIcon.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        usernameIcon.widthAnchor.constraint(equalToConstant: 20).isActive = true
         
+        let usernameInput = UITextField()
+        usernameInput.translatesAutoresizingMaskIntoConstraints = false
+        usernameInput.attributedPlaceholder = NSAttributedString(string: "Username",
+                                                                 attributes: [NSAttributedString.Key.foregroundColor: LPSColors.white34])
+        usernameInput.font = LPSFonts.quicksandMedium.of(size: 16.0)
+        usernameInput.autocapitalizationType = .none
+        usernameInput.spellCheckingType = .no
+        usernameInput.keyboardType = .emailAddress
+        usernameInput.textColor = LPSColors.white
+        view.addSubview(usernameInput)
+        usernameInput.topAnchor.constraint(equalTo: usernameIcon.topAnchor).isActive = true
+        usernameInput.bottomAnchor.constraint(equalTo: usernameIcon.bottomAnchor).isActive = true
+        usernameInput.leadingAnchor.constraint(equalTo: usernameIcon.trailingAnchor, constant: 8).isActive = true
+        usernameInput.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        let usernameSeparator = UIView()
+        usernameSeparator.translatesAutoresizingMaskIntoConstraints = false
+        usernameSeparator.backgroundColor = LPSColors.white34
+        view.addSubview(usernameSeparator)
+        usernameSeparator.topAnchor.constraint(equalTo: usernameIcon.bottomAnchor, constant: 8).isActive = true
+        usernameSeparator.leadingAnchor.constraint(equalTo: usernameIcon.leadingAnchor).isActive = true
+        usernameSeparator.trailingAnchor.constraint(equalTo: usernameInput.trailingAnchor).isActive = true
+        usernameSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        let passwordIcon = UIImageView()
+        let lockIcon = MaterialDesignSymbol(text:MaterialDesignIcon.lock24px, size:20)
+        lockIcon.addAttribute(attributeName: NSAttributedString.Key.foregroundColor, value: UIColor.white)
+        passwordIcon.image = lockIcon.image(size: CGSize(width: 20, height: 20))
+        passwordIcon.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(passwordIcon)
+        passwordIcon.topAnchor.constraint(equalTo: usernameSeparator.bottomAnchor, constant: 18).isActive = true
+        passwordIcon.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        passwordIcon.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        passwordIcon.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        let passwordInput = UITextField()
+        passwordInput.translatesAutoresizingMaskIntoConstraints = false
+        passwordInput.attributedPlaceholder = NSAttributedString(string: "Password",
+                                                                 attributes: [NSAttributedString.Key.foregroundColor: LPSColors.white34])
+        passwordInput.font = LPSFonts.quicksandMedium.of(size: 16.0)
+        passwordInput.autocapitalizationType = .none
+        passwordInput.keyboardType = .asciiCapable
+        passwordInput.isSecureTextEntry = true
+        passwordInput.textColor = LPSColors.white
+        view.addSubview(passwordInput)
+        passwordInput.topAnchor.constraint(equalTo: passwordIcon.topAnchor).isActive = true
+        passwordInput.bottomAnchor.constraint(equalTo: passwordIcon.bottomAnchor).isActive = true
+        passwordInput.leadingAnchor.constraint(equalTo: passwordIcon.trailingAnchor, constant: 8).isActive = true
+        passwordInput.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        let passwordSeparator = UIView()
+        passwordSeparator.translatesAutoresizingMaskIntoConstraints = false
+        passwordSeparator.backgroundColor = LPSColors.white34
+        view.addSubview(passwordSeparator)
+        passwordSeparator.topAnchor.constraint(equalTo: passwordIcon.bottomAnchor, constant: 8).isActive = true
+        passwordSeparator.leadingAnchor.constraint(equalTo: passwordIcon.leadingAnchor).isActive = true
+        passwordSeparator.trailingAnchor.constraint(equalTo: passwordInput.trailingAnchor).isActive = true
+        passwordSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        let securityCodeIcon = UIImageView()
+        let thumbIcon = UIImage(named: "ic_fingerprint")
+        securityCodeIcon.image = thumbIcon
+        securityCodeIcon.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(securityCodeIcon)
+        securityCodeIcon.topAnchor.constraint(equalTo: passwordSeparator.bottomAnchor, constant: 18).isActive = true
+        securityCodeIcon.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        securityCodeIcon.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        securityCodeIcon.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        let securityCodeInput = UITextField()
+        securityCodeInput.translatesAutoresizingMaskIntoConstraints = false
+        securityCodeInput.attributedPlaceholder = NSAttributedString(string: "Security Code",
+                                                                 attributes: [NSAttributedString.Key.foregroundColor: LPSColors.white34])
+        securityCodeInput.font = LPSFonts.quicksandMedium.of(size: 16.0)
+        securityCodeInput.autocapitalizationType = .none
+        securityCodeInput.keyboardType = .asciiCapable
+        securityCodeInput.isSecureTextEntry = true
+        securityCodeInput.textColor = LPSColors.white
+        view.addSubview(securityCodeInput)
+        securityCodeInput.topAnchor.constraint(equalTo: securityCodeIcon.topAnchor).isActive = true
+        securityCodeInput.bottomAnchor.constraint(equalTo: securityCodeIcon.bottomAnchor).isActive = true
+        securityCodeInput.leadingAnchor.constraint(equalTo: securityCodeIcon.trailingAnchor, constant: 8).isActive = true
+        securityCodeInput.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        let securityCodeSeparator = UIView()
+        securityCodeSeparator.translatesAutoresizingMaskIntoConstraints = false
+        securityCodeSeparator.backgroundColor = LPSColors.white34
+        view.addSubview(securityCodeSeparator)
+        securityCodeSeparator.topAnchor.constraint(equalTo: securityCodeIcon.bottomAnchor, constant: 8).isActive = true
+        securityCodeSeparator.leadingAnchor.constraint(equalTo: securityCodeIcon.leadingAnchor).isActive = true
+        securityCodeSeparator.trailingAnchor.constraint(equalTo: securityCodeInput.trailingAnchor).isActive = true
+        securityCodeSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        let signInRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.signInButtonClicked(sender:)))
+        
+        let signInButton = UIButton()
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.setTitle("SIGN IN", for: .normal)
+        signInButton.layer.cornerRadius = 4
+        signInButton.backgroundColor = LPSColors.primary
+        signInButton.addGestureRecognizer(signInRecognizer)
+        view.addSubview(signInButton)
+        signInButton.topAnchor.constraint(equalTo: securityCodeSeparator.bottomAnchor, constant: 32).isActive = true
+        signInButton.leadingAnchor.constraint(equalTo: securityCodeSeparator.leadingAnchor).isActive = true
+        signInButton.trailingAnchor.constraint(equalTo: securityCodeSeparator.trailingAnchor).isActive = true
+        signInButton.heightAnchor.constraint(equalToConstant: 48).isActive = true
     }
     
     
--- MyTimeSW/Utilities/LPSColors.swift
+++ MyTimeSW/Utilities/LPSColors.swift
@@ -12,7 +12,10 @@ import UIKit
 class LPSColors {
     static let primary = UIColor(red: 33/255, green: 150/255, blue: 243/255, alpha: 1)
     
+    static let loginBackgroundGradientStart = UIColor(red: 36/255, green: 59/255, blue: 85/255, alpha: 1)
+    static let loginBackgroundGradientEnd = UIColor(red: 20/255, green: 30/255, blue: 48/255, alpha: 1)
+    
     static let black = UIColor.black
     static let white = UIColor.white
-    static let white34 = UIColor(white: 0.34, alpha: 1)
+    static let white34 = UIColor(white: 1, alpha: 0.34)
 }
